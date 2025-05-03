75afc1110b1eed3d386b5c162633f50b09975ac5
Malin Hööglund <malin0hoog@gmail.com>
2025-05-03 12:52:52 +0200

    commiting to save, a few things not working, will work on something else for now

--- js/page1.js
+++ js/page1.js
@@ -13,22 +13,23 @@ addMdToPage(`<br>`);
 
 let studySatisfaction = await dbQuery(
   "SELECT profession AS Profession, " +
+  "gender AS Gender, " +
   "CASE " +
   "WHEN study_satisfaction IN (1, 2) THEN 'Low (1-2)' " +
   "WHEN study_satisfaction = 3 THEN 'Medium (3)' " +
   "WHEN study_satisfaction IN (4, 5) THEN 'High (4-5)' " +
   "END AS Satisfaction_Level, " +
   "COUNT(*) AS Amount_Of_Students, " +
-  "ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession), 1) AS Percentage " +
+  "ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession, gender), 1) AS Percentage " +
   "FROM results " +
-  "WHERE study_satisfaction != 0 " +
-  "GROUP BY profession, Satisfaction_Level " +
-  "ORDER BY profession, " +
+  "WHERE study_satisfaction != 0 AND study_satisfaction IS NOT NULL AND gender IS NOT NULL " +
+  "GROUP BY profession, gender, Satisfaction_Level " +
+  "ORDER BY profession, gender, " +
   "CASE " +
-  "WHEN Satisfaction_Level = 'Low (1-2)' THEN 3 " +
-  "WHEN Satisfaction_Level = 'Medium (3)' THEN 2 " +
-  "WHEN Satisfaction_Level = 'High (4-5)' THEN 1 " +
-  "END "
+  "WHEN study_satisfaction IN (4, 5) THEN 1 " +
+  "WHEN study_satisfaction = 3 THEN 2 " +
+  "WHEN study_satisfaction IN (1, 2) THEN 3 " +
+  "END"
 );
 
 tableFromData({ data: studySatisfaction });
@@ -37,17 +38,60 @@ addMdToPage(`<br>`);
 
 let studentDepression = await dbQuery(
   "SELECT profession AS Profession, " +
+  "gender AS Gender, " +
   "CASE " +
   "WHEN depression = 0 THEN 'Does Not Feel Depressed' " +
   "WHEN depression = 1 THEN 'Does Feel Depressed' " +
   "END AS Depression_Level, " +
   "COUNT(*) AS Amount_Of_Students, " +
-  "ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession), 1) AS Percentage " +
+  "ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession, gender), 1) AS Percentage " +
   "FROM results " +
-  "WHERE depression IS NOT NULL " +
-  "GROUP BY profession, Depression_Level " +
-  "ORDER BY profession, Depression_Level"
+  "WHERE depression IS NOT NULL AND gender IS NOT NULL " +
+  "GROUP BY profession, gender, Depression_Level " +
+  "ORDER BY profession, gender, Depression_Level"
 );
 
-
 tableFromData({ data: studentDepression });
+
+/*
+let studentDepression2 = await dbQuery(
+  "SELECT profession AS Profession, " +
+  "gender AS Gender, " +
+  "CASE " +
+  "WHEN depression = 0 THEN 'Does Not Feel Depressed' " +
+  "WHEN depression = 1 THEN 'Does Feel Depressed' " +
+  "END AS Depression_Level, " +
+  "ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession, gender), 1) AS Percentage " +
+  "FROM results " +
+  "WHERE depression IS NOT NULL AND gender IS NOT NULL " +
+  "GROUP BY profession, gender, Depression_Level " +
+  "ORDER BY profession, gender, Depression_Level"
+);
+
+studentDepression2 = studentDepression2.filter(row =>
+  typeof row.Percentage === 'number' && !isNaN(row.Percentage)
+);
+
+drawGoogleChart({
+  type: 'ColumnChart',
+  data: makeChartFriendly(studentDepression2, 'Profession', 'Percentage', 'Depression_Level'),
+  options: {
+    title: 'Depression Level by Profession and Gender',
+    animation: {
+      startup: true,
+      duration: 1000,
+      easing: 'out'
+    },
+    height: 600,
+    chartArea: { left: 75, bottom: 150, width: '85%' },
+    legend: { position: 'top' },
+    vAxis: { title: 'Percentage (%)' },
+    hAxis: {
+      title: 'Profession',
+      slantedText: true,
+      slantedTextAngle: 45
+    },
+    colors: ['#66bb6a', '#ef5350'] // grönt = mår bra, rött = deprimerad
+  }
+});
+*/
