f2bb1de5880e89e9e0f18350b40ac8e877592649
Malin Hööglund <malin0hoog@gmail.com>
2025-05-04 17:27:57 +0200

    working on cgpa and sleeping habits dropdown

--- js/page2.js
+++ js/page2.js
@@ -1,6 +1,127 @@
 addMdToPage(`
   ## CPGA
+
+  ________________
   `);
+
+addMdToPage(`<br>`);
+
+let cgpa = addDropdown('Check the sleeping habits based on students CGPA', ['Below 6.0', 'Between 6 and 7', 'Between 7 and 8', 'Between 8 and 9', 'Above 9', 'All CGPA']);
+
+let sleepingHabits = await dbQuery(`
+  SELECT sleep_duration, COUNT(*) AS Amount_Of_Students, 
+  ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession), 1) AS Percentage
+  FROM results
+  WHERE sleep_duration != 'Others'
+  GROUP BY sleep_duration
+  ORDER BY 
+  CASE
+  WHEN sleep_duration = 'Less than 5 hours' THEN 1
+  WHEN sleep_duration = '5-6 hours' THEN 2
+  WHEN sleep_duration = '7-8 hours' THEN 3
+  WHEN sleep_duration = 'More than 8 hours' THEN 4
+  END;
+  `);
+
+let sleepingHabits1 = await dbQuery(`
+  SELECT sleep_duration, COUNT(*) AS Amount_Of_Students, 
+  ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession), 1) AS Percentage
+  FROM results
+  WHERE sleep_duration != 'Others'
+  AND cgpa < 6 
+  GROUP BY sleep_duration
+  ORDER BY 
+  CASE
+  WHEN sleep_duration = 'Less than 5 hours' THEN 1
+  WHEN sleep_duration = '5-6 hours' THEN 2
+  WHEN sleep_duration = '7-8 hours' THEN 3
+  WHEN sleep_duration = 'More than 8 hours' THEN 4
+  END;
+  `);
+
+let sleepingHabits2 = await dbQuery(`
+  SELECT sleep_duration, COUNT(*) AS Amount_Of_Students, 
+  ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession), 1) AS Percentage
+  FROM results
+  WHERE sleep_duration != 'Others'
+  AND cgpa BETWEEN 6 and 7
+  GROUP BY sleep_duration
+  ORDER BY 
+  CASE
+  WHEN sleep_duration = 'Less than 5 hours' THEN 1
+  WHEN sleep_duration = '5-6 hours' THEN 2
+  WHEN sleep_duration = '7-8 hours' THEN 3
+  WHEN sleep_duration = 'More than 8 hours' THEN 4
+  END;
+  `);
+
+let sleepingHabits3 = await dbQuery(`
+  SELECT sleep_duration, COUNT(*) AS Amount_Of_Students, 
+  ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession), 1) AS Percentage
+  FROM results
+  WHERE sleep_duration != 'Others'
+  AND cgpa BETWEEN 7 and 8
+  GROUP BY sleep_duration
+  ORDER BY 
+  CASE
+  WHEN sleep_duration = 'Less than 5 hours' THEN 1
+  WHEN sleep_duration = '5-6 hours' THEN 2
+  WHEN sleep_duration = '7-8 hours' THEN 3
+  WHEN sleep_duration = 'More than 8 hours' THEN 4
+  END;
+  `);
+
+let sleepingHabits4 = await dbQuery(`
+  SELECT sleep_duration, COUNT(*) AS Amount_Of_Students, 
+  ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession), 1) AS Percentage
+  FROM results
+  WHERE sleep_duration != 'Others'
+  AND cgpa BETWEEN 8 and 9
+  GROUP BY sleep_duration
+  ORDER BY 
+  CASE
+  WHEN sleep_duration = 'Less than 5 hours' THEN 1
+  WHEN sleep_duration = '5-6 hours' THEN 2
+  WHEN sleep_duration = '7-8 hours' THEN 3
+  WHEN sleep_duration = 'More than 8 hours' THEN 4
+  END;
+  `);
+
+let sleepingHabits5 = await dbQuery(`
+  SELECT sleep_duration, COUNT(*) AS Amount_Of_Students, 
+  ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession), 1) AS Percentage
+  FROM results
+  WHERE sleep_duration != 'Others'
+  AND cgpa > 9
+  GROUP BY sleep_duration
+  ORDER BY 
+  CASE
+  WHEN sleep_duration = 'Less than 5 hours' THEN 1
+  WHEN sleep_duration = '5-6 hours' THEN 2
+  WHEN sleep_duration = '7-8 hours' THEN 3
+  WHEN sleep_duration = 'More than 8 hours' THEN 4
+  END;
+  `);
+
+
+
+/*
+drawGoogleChart({
+  type: 'PieChart',
+  data: makeChartFriendly(sleepingHabits1),
+  options: {
+    title: 'Sleeping Habits',
+    height: 600,
+    vAxis: { title: 'ssometing' },
+    hAxis: { title: 'sometj' },
+    colors: ['#b3e0ff', '#80ccff', '#1aa3ff', '006bb3'] // olika nyanser av blå
+  }
+});
+*/
+
+addMdToPage(`__________`);
+
+
 
 // sorterar bort others i diet
 
