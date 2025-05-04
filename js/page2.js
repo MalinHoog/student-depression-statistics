775974cc1e1266045a62089eebe9e4b6086a5e5e
Malin Hööglund <malin0hoog@gmail.com>
2025-05-04 15:39:34 +0200

    starting to work on more tabels

--- js/page2.js
+++ js/page2.js
@@ -2,7 +2,38 @@ addMdToPage(`
   ## CPGA
   `);
 
+// sorterar bort others i diet
+let foodhabits = await dbQuery(`
+SELECT 
+  profession AS Profession,
+  COUNT(*) AS Amount_Of_Students,
+  CASE
+    WHEN dietary_habits = 'Healthy' THEN 'Healthy Diet'
+    WHEN dietary_habits = 'Moderate' THEN 'Moderate Diet'
+    WHEN dietary_habits = 'Unhealthy' THEN 'Unhealthy Diet'
+  END AS Diet_Habits,
+  ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY profession), 1) AS Percentage
+FROM results
+WHERE dietary_habits != 'Others'
+GROUP BY profession, dietary_habits
+ORDER BY profession, dietary_habits;
+  `);
 
+tableFromData({ data: foodhabits })
+/*
+drawGoogleChart({
+  type: 'ColumnChart',
+  data: makeChartFriendly(foodhabits, 'CGPA-intervall', 'Genomsnittlig depression'),
+  options: {
+    title: 'Samband mellan CGPA och depression',
+    height: 400,
+    vAxis: { title: 'Depression (0–1)' },
+    hAxis: { title: 'CGPA-intervall' },
+    colors: ['#3366cc']
+  }
+});
+*/
+addMdToPage(`<br>`)
 
 let cgpaData = await dbQuery(`
   SELECT 
