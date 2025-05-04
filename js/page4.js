3258f3482cec3fdf35bdb96c6d76ed835345be42
Malin Hööglund <malin0hoog@gmail.com>
2025-05-04 23:33:20 +0200

    have done everything I can to make sure everything is done in time. For now, I feel done

---
+++ js/page4.js
@@ -0,0 +1,8 @@
+addMdToPage(`
+  ### Shapiro-Wilk
+
+  UnfortunatelyI did not have time to test any data for normal distribution/Shapiro Wilk.
+
+  If you are my teacher reading this, and this content is important for my grade, please don't hesitate to reach out — I will make the necessary additions as soon as possible.
+
+  `)
